b4b964581d21ac010a7d486ef21e0e492840641d
Lukas Nabergall <lnabergall@mail.usf.edu>
2017-05-29 22:30:41 -0400

    Fixed incorrect pattern labeling

--- insertion_detector.py
+++ insertion_detector.py
@@ -8,48 +8,29 @@ from itertools import product, permutations
 from objects import Word
 
 
-def generate_insertions(word, pattern_instance, size_limit):
-    new_letters = list(range(1, size_limit + 1))
-    new_letters = [str(letter) for letter in new_letters 
-                   if str(letter) not in word]
-    instance_letters = list(set(pattern_instance.replace("...", "")))
-    instance_letters.sort()
+def generate_insertions(word, pattern_instance):
     pattern_parts = pattern_instance.split("...")
-    return_word = pattern_parts[0] == pattern_parts[1][::-1]
     insertions = set()
     insertion_indices = {}
-    instances = []
-
-    for indices in permutations(new_letters, len(instance_letters)):
-        for i in range(len(pattern_parts)):
-            relabeled_part = ""
-            for j in range(len(instance_letters)):
-                relabeled_part += indices[j]
-            if return_word and i == 1:
-                relabeled_part = relabeled_part[::-1]   # Reverses relabeled_part
-            pattern_parts[i] = relabeled_part
-        instances.append(pattern_parts[:])
-
-    for instance in instances:
-        for i, j in product(range(len(word)+1), range(len(word)+1)):
-            if i < j:
-                new_word = Word(word[:i] + instance[0] + word[i:j] 
-                                + instance[1] + word[j:])
-                insertion_indices[new_word] = (i, j)
-            else:
-                new_word = Word(word[:j] + instance[1] + word[j:i]
-                                + instance[0] + word[i:])
-                insertion_indices[new_word] = (j, i)
-            insertions.add(new_word)
+    for i, j in product(range(len(word)+1), range(len(word)+1)):
+        if i < j:
+            new_word = Word(word[:i] + pattern_parts[0] + word[i:j] 
+                            + pattern_parts[1] + word[j:])
+            insertion_indices[new_word] = (i, j)
+        else:
+            new_word = Word(word[:j] + pattern_parts[1] + word[j:i]
+                            + pattern_parts[0] + word[i:])
+            insertion_indices[new_word] = (j, i)
+        insertions.add(new_word)
 
     return insertions, insertion_indices
 
 
-def find_adjacent_vertices(word, repeat_word, return_word, size_limit):
+def find_adjacent_vertices(word, repeat_word, return_word):
     repeat_neighbors, repeat_insertion_indices = generate_insertions(
-        word, repeat_word, size_limit)
+        word, repeat_word)
     return_neighbors, return_insertion_indices = generate_insertions(
-        word, return_word, size_limit)
+        word, return_word)
 
     return (repeat_neighbors, repeat_insertion_indices, 
             return_neighbors, return_insertion_indices)
@@ -70,20 +51,14 @@ def is_insertion(word1, word2):
     if len(word1) == len(word2):
         return word1 == word2
     else:
-        size_limit = word2.size
-        insertion_size = word2.size - word1.size
-        if insertion_size < 10:
-            pattern_part = "".join([str(i) for i in range(insertion_size)])
-        else:
-            pattern_part = "".join([str(i) for i in range(10)])
-            for i in range(insertion_size-9):
-                pattern_part += chr(i+97)
-
+        insertion_letters = list(set(word2) - set(word1))
+        insertion_letters.sort()
+        pattern_part = "".join(insertion_letters)
         repeat_word = pattern_part + "..." + pattern_part
         return_word = pattern_part + "..." + pattern_part[::-1]
         (repeat_neighbors, repeat_insertion_indices, 
          return_neighbors, return_insertion_indices) = find_adjacent_vertices(
-            word1, repeat_word, return_word, size_limit)
+            word1, repeat_word, return_word)
         if word2 in repeat_neighbors:
             return True, repeat_insertion_indices[word2]
         elif word2 in return_neighbors:
