1e7308f6f04fa48dd2fac9f483f115b26f1486a1
Lukas Nabergall <lnabergall@mail.usf.edu>
2017-09-13 15:59:07 -0400

    Restructured the homology API, especially input/output, and added some documentation

---
+++ homology/io.py
@@ -0,0 +1,83 @@
+"""
+Input/output functions for the homology API---in particular, for loading
+maximal simplices and storing homology data.
+
+Functions:
+
+    get_word_labels, retrieve_maximal_simplices, store_homology_data
+"""
+
+from word_explorer.io import retrieve_data, store_data
+from word_explorer.word_graphs.word_graph import expand_word_graph
+from word_explorer.word_graphs.io import (retrieve_word_graph,
+                                          retrieve_word_subgraphs,
+                                          get_word_graph_filename)
+
+
+FACE_SQUARE_STRUCTURES = ("((0, 1), (1, 2), (3, 2), (0, 3))",
+                          "((0, 1), (1, 2), (2, 3), (0, 3))", 
+                          "((0, 1), (2, 1), (3, 2), (0, 3))")
+
+
+def get_word_labels(expanded_word_graph):
+    words = sorted(list(expanded_word_graph))
+    return {word: i for i, word in enumerate(words)}
+
+
+def retrieve_maximal_simplices(ascending_order, size, 
+                               name_base="word_graph_size", name_suffix="",
+                               max_simplex_types=["triangle"]):
+    maximal_simplices = []
+    for subgraph_type in max_simplex_types:
+        # Add maximal k-simplices, k >= 2
+        if subgraph_type != "square":
+            maximal_simplices.extend(retrieve_word_subgraphs(
+                ascending_order, size, subgraph_type, 
+                name_base, name_suffix))
+        else:
+            simplices = retrieve_word_subgraphs(
+                ascending_order, size, subgraph_type, 
+                name_base, name_suffix, sorted_=True)
+            for subgraph_class, simplex_list in simplices.items():
+                if subgraph_class in FACE_SQUARE_STRUCTURES:
+                    maximal_simplices.extend(simplex_list)
+        word_graph = retrieve_word_graph(
+            ascending_order, size, name_base, name_suffix)
+        words = sorted(list(expand_word_graph(word_graph)))
+        word_labels = {word: i for i, word in enumerate(words)}
+
+        # Add maximal 1-simplices (edges)
+        for vertex, neighbors in word_graph.items():
+            for neighbor in neighbors:
+                for subgraph in maximal_simplices:
+                    if vertex in subgraph and neighbor in subgraph:
+                        break
+                else:
+                    maximal_simplices.append([vertex, neighbor])
+
+    # Label simplices
+    labeled_maximal_simplices = []
+    for subgraph in maximal_simplices:
+        simplex = [word_labels[word] for word in subgraph]
+        labeled_maximal_simplices.append(simplex)
+
+    return labeled_maximal_simplices
+
+
+def store_homology_data(ascending_order, size, homology_data, 
+                        max_simplex_types, name_base="word_graph_size",
+                        name_suffix=""):
+    file_name = get_word_graph_filename(ascending_order, size, 
+                                        name_base, name_suffix)[:-4]
+    file_name += "_homology_" + "_".join(max_simplex_types) + ".txt"
+    print_data = []
+    for i, homology_group in enumerate(homology_data):
+        print_data.append("\nBetti number " + i + " : " + homology_group[0])
+        if len(homology_group) > 1:
+            print_data.append("\nBoundary matrix of dimension " + i)
+            print_data.append(homology_group[1])
+            print_data.append("\nReduced boundary matrix of dimension " + i)
+            print_data.append(homology_group[2])
+        print_data.append("\n--------------------------\n")
+
+    store_data(print_data, file_name)
--- homology/word_graph_homology.py
+++ homology/word_graph_homology.py
@@ -4,7 +4,7 @@ corresponding to word graphs.
 
 Functions:
 
-    get_word_labels, get_maximal_simplices, main
+    recursion_handler, calculate_homology_groups
 """
 
 import os
@@ -14,107 +14,40 @@ import threading
 import numpy as np
 from simplex import SimplicialComplex
 
-from word_explorer.objects import Word
-from word_explorer.word_graphs.subgraph_finder import (
-    extract_word_graph, expand_word_graph)
-
- 
-FACE_SQUARE_STRUCTURES = ("((0, 1), (1, 2), (3, 2), (0, 3))",
-                          "((0, 1), (1, 2), (2, 3), (0, 3))", 
-                          "((0, 1), (2, 1), (3, 2), (0, 3))")
+from .io import retrieve_maximal_simplices, store_homology_data
 
 
-def get_word_labels(file_path):
-    with open(file_path + ".txt", "r") as graph_file:
-        word_graph = expand_word_graph(extract_word_graph(graph_file, Word))
-    words = sorted(list(word_graph))
-    return {word: i for i, word in enumerate(words)}
+def recursion_handler(function):
+    def function_wrapper(*args, **kwargs):
+        threading.stack_size(67108864) # 64MB stack
+        sys.setrecursionlimit(2**22) # something very big, 64MB limit hit first
+
+        # only new threads get the redefined stack size
+        thread = threading.Thread(target=function, args=args, kwargs=kwargs)
+        thread.start()
+
+    return function_wrapper
 
 
-def get_maximal_simplices(file_path, word_labels, max_simplex_types=["triangle"]):
-    maximal_simplices = []
-    if "triangle" in max_simplex_types:
-        triangles = []
-        with open(file_path + "_triangles.txt", "r") as triangle_file:
-            for line in triangle_file:
-                if line.startswith("(") or line.startswith("["):
-                    triangle_string = line.strip()[2:-2].split("', '")
-                    triangle = [Word(word) for word in triangle_string]
-                    triangles.append(triangle)
-        maximal_simplices.extend(triangles)
-
-    if "square" in max_simplex_types:
-        # Collect parallel + (3,1) squares
-        directed_structure = None
-        squares = []
-        with open(file_path + "_squares_sorted.txt", "r") as sorted_squares_file:
-            for line in sorted_squares_file:
-                if line.strip().endswith("):"):
-                    directed_structure = line.strip()[:-1]
-                if (directed_structure in FACE_SQUARE_STRUCTURES 
-                        and line.startswith("('")):
-                    square_string = line.strip()[2:-2].split("', '")
-                    square = [Word(word) for word in square_string]
-                    squares.append(square)
-        maximal_simplices.extend(squares)
-
-    # Get maximal 1-simplices (edges)
-    with open(file_path + ".txt", "r") as graph_file:
-        word_graph = expand_word_graph(extract_word_graph(graph_file, Word))
-    for vertex, neighbors in word_graph.items():
-        for neighbor in neighbors:
-            maximal = True
-            for subgraph in maximal_simplices:
-                if vertex in subgraph and neighbor in subgraph:
-                    maximal = False
-                    break
-            if maximal:
-                maximal_simplices.append([vertex, neighbor])
-
-    relabeled_maximal_simplices = []
-    for subgraph in maximal_simplices:
-        simplex = [word_labels[word] for word in subgraph]
-        relabeled_maximal_simplices.append(simplex)
-
-    return relabeled_maximal_simplices
-
-
-def main():
-    print("This script calculates the homology of a simplicial complex"
-          + " corresponding to a given ascending order word graph or subgraph.\n")
-    size = int(input("Word graph size? ").strip())
-    graph_type = input("Word graph or subgraph? ")
-    if graph_type.strip().lower().startswith("s"):
-        word_graph_file_name = input("Enter name of file containing a word subgraph" 
-                                     + "(excluding extension): ").strip()
-        file_path = os.path.join(os.pardir, word_graph_file_name)
-    else:
-        file_prefix = "aoword_graph_size"
-        file_path = os.path.join(os.pardir, file_prefix + str(size))
-    word_labels = get_word_labels(file_path)
-    maximal_simplices = get_maximal_simplices(
-        file_path, word_labels, max_simplex_types=["triangle"])
-    print("Maximal 1-simplices:", len([simplex for simplex in maximal_simplices 
-                                       if len(simplex) == 2]))
+@recursion_handler
+def calculate_homology_groups(ascending_order, size, max_homology_group=3,
+                              max_simplex_types=["triangle"],
+                              name_base="word_graph_size", name_suffix="",
+                              output_boundary_matrices=False):
+    maximal_simplices = retrieve_maximal_simplices(
+        ascending_order, size, name_base, name_suffix, max_simplex_types)
+    maximal_1simplices_count = len([simplex for simplex in maximal_simplices
+                                    if len(simplex) == 2])
     simplicial_complex = SimplicialComplex(maximal_simplices)
-    with open(file_path + "_homology_triangle.txt", "w") as homology_file:
-        np.set_printoptions(threshold=100000000000)
-        for i in range(4):
-            # boundary_matrix = simplicial_complex.get_boundary_matrix(i, 2)
-            # print("\nBoundary matrix of dimension", i, file=homology_file)
-            # print(boundary_matrix, file=homology_file)
-            # print("\nReduced boundary matrix of dimension", i, file=homology_file)
-            # print(simplicial_complex.reduce_matrix(boundary_matrix)[0], 
-            #       file=homology_file)
-            print("\nBetti number", i, "=", simplicial_complex.betti_number(i, 2), 
-                  file=homology_file)
-            print("\n--------------------------\n", file=homology_file)
 
+    homology_group_data = []
+    for i in range(max_homology_group+1):
+        homology_group_data.append([simplicial_complex.betti_number(i, 2)])
+        if output_boundary_matrices:
+            boundary_matrix = simplicial_complex.get_boundary_matrix(i, 2)
+            reduced_matrix = simplicial_complex.reduce_matrix(boundary_matrix)[0]
+            homology_group_data[-1].extend([boundary_matrix, reduced_matrix])
+        homology_group_data[-1] = tuple(homology_group_data[-1])
 
-if __name__ == '__main__':
-    threading.stack_size(67108864) # 64MB stack
-    sys.setrecursionlimit(2**22) # something real big, 64MB limit hit first
-
-    # only new threads get the redefined stack size
-    thread = threading.Thread(target=main)
-    thread.start()
+    store_homology_data(ascending_order, size, homology_group_data, 
+                        max_simplex_types, name_base, name_suffix)
